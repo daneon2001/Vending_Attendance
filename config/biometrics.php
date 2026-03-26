39849fabb5382f72341ec2912a00e6873dea3fa4
Daniel Andrade <daneon2001@gmail.com>
2026-03-26 16:29:34 -0600

    actualzacion de api's de enrolamiento

--- config/biometrics.php
+++ config/biometrics.php
@@ -4,6 +4,13 @@ return [
     'fingerprint' => [
         'default_vendor' => env('BIOMETRIC_FINGERPRINT_VENDOR', 'digitalpersona'),
         'default_source' => env('BIOMETRIC_FINGERPRINT_SOURCE', 'scanner'),
+        'enrolment_allowed_formats' => [
+            'DPFP_PROPRIETARY',
+            'zkteco-v1',
+        ],
+        'legacy_compatible_formats' => [
+            'DPFP_PROPRIETARY',
+        ],
     ],
     'face' => [
         'default_vendor' => env('BIOMETRIC_FACE_VENDOR', 'digitalpersona'),
